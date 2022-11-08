b453f10e0fa5813d7de8d4d3b66598b34244018f
willian <wruizza@cesde.net>
2022-11-08 18:13:34 -0500

    modifico cambios ya funciona se pone verde

--- src/app/components/tarjetas/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjetas/tarjeta-credito/tarjeta-credito.component.ts
@@ -3,26 +3,36 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-tarjeta-credito',
   templateUrl: './tarjeta-credito.component.html',
-  styleUrls: ['./tarjeta-credito.component.css']
+  styleUrls: ['./tarjeta-credito.component.css'],
 })
 export class TarjetaCreditoComponent implements OnInit {
   form: FormGroup;
   constructor(private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
       id: 0,
-      titular: ["", [Validators.required]],
-      numeroTarjeta: ["", [Validators.required, Validators.maxLength(16), Validators.minLength(16)]],
-      fechaExpiracion: ["", [Validators.required, Validators.maxLength(16), Validators.minLength(16)]],
-      cvv: ["", [Validators.required, Validators.maxLength(3), Validators.minLength(3)]],
-
+      titular: ['', [Validators.required]],
+      numeroTarjeta: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(16),
+          Validators.minLength(16),
+        ],
+      ],
+      fechaExpiracion: [
+        '',
+        [Validators.required, Validators.maxLength(5), Validators.minLength(5)],
+      ],
+      cvv: [
+        '',
+        [Validators.required, Validators.maxLength(3), Validators.minLength(3)],
+      ],
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   guardarTarjeta() {
     console.log(this.form);
   }
-
 }
