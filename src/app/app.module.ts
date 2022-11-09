f0493ab76cf7706a3b7df101bed8f54ffbd768ba
willian <wruizza@cesde.net>
2022-11-09 15:41:49 -0500

    se hace metodo agreagar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
@@ -16,14 +16,14 @@ import { ReactiveFormsModule } from '@angular/forms';
     TarjetaCreditoComponent,
     ListarTarjetaCreditoComponent,
     FooterComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
